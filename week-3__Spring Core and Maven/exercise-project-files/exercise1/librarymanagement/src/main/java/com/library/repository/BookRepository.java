7b5db37cd46b89ed3e2dfbb2181eb29a03e4f43d
Swetha S <122204898+swethasakthivel05@users.noreply.github.com>
2025-07-06 17:27:42 +0530

    Delete week-3__Spring Core and Maven directory

--- week-3__Spring Core and Maven/exercise-project-files/exercise1/librarymanagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void saveBook(String title) {
-        System.out.println("Book '" + title + "' saved to repository.");
-    }
-}
--- week-3__Spring Core and Maven/exercise-project-files/exercise2/librarymanagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void save(String bookName) {
-        System.out.println("Book saved: " + bookName);
-    }
-}
